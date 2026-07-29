1e56422d31740d7828f88ca449b660e2f3e8b2d5
Your Name <you@example.com>
2026-07-29 10:41:15 -0400

    fix: define PyroWave format constants in PyroWaveRenderer.h (submodule dependency)

--- Limelight/Stream/PyroWaveRenderer.h
+++ Limelight/Stream/PyroWaveRenderer.h
@@ -1,6 +1,16 @@
 #import <UIKit/UIKit.h>
 #include "Limelight.h"
 
+// PyroWave video format constants — defined here so VideoDecoderRenderer.m
+// can use them without depending on an unreleased moonlight-common-c revision.
+#ifndef VIDEO_FORMAT_MASK_PYROWAVE
+#define VIDEO_FORMAT_PYROWAVE             0x00010000
+#define VIDEO_FORMAT_PYROWAVE_MAIN10      0x00020000
+#define VIDEO_FORMAT_PYROWAVE_HIGH8_444   0x00040000
+#define VIDEO_FORMAT_PYROWAVE_HIGH10_444  0x00080000
+#define VIDEO_FORMAT_MASK_PYROWAVE        0x000F0000
+#endif
+
 @interface PyroWaveRenderer : NSObject
 
 - (id)initWithView:(UIView*)view;
